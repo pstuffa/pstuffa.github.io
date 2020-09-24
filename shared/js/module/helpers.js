8248ab22f9ea07aaaff40b0c1ed3f5224c7813d4
pstuffa <p.buffa@gmail.com>
2020-09-23 19:23:37 -0600

    updated

--- shared/js/module/helpers.js
+++ shared/js/module/helpers.js
@@ -36,3 +36,92 @@ const lineData = (n) => {
   return points;
 
 }
+
+
+function getDates(startDate, stopDate) {
+  var dateArray = [];
+  var currentDate = moment(startDate);
+  var stopDate = moment(stopDate);
+  while (currentDate <= stopDate) {
+      dateArray.push( moment(currentDate).format('MM-DD-YYYY') )
+      currentDate = moment(currentDate).add(1, 'days');
+  }
+  return dateArray;
+}
+
+function movingAverage(values, N) {
+let i = 0;
+let sum = 0;
+const means = new Float64Array(values.length).fill(NaN);
+for (let n = Math.min(N - 1, values.length); i < n; ++i) {
+  sum += values[i];
+}
+for (let n = values.length; i < n; ++i) {
+  sum += values[i];
+  means[i] = sum / N;
+  sum -= values[i - N + 1];
+}
+return means;
+}
+
+function movingAvgData(dateArray, byDateDays, N) {
+
+const input = dateArray.map(d => {
+  return {
+    date: d,
+    value: byDateDays[`$${d}`] == undefined ? 0 : byDateDays[`$${d}`]
+  }
+})
+
+const movingAvg = movingAverage(input.map(d => d.value), N)
+
+const returnObject = {};
+
+dateArray.forEach((date,i) => {
+  returnObject[`$${date}`] = movingAvg[i]
+})
+
+return returnObject
+}
+
+function generateCumulative (mergedDenver, byDate, mvData) {
+const returnArray = [];
+
+const nest = d3.nest()
+  .key(d => d.year)
+  .entries(mergedDenver)
+
+const lyLookup = {}
+byDate.forEach(year => {
+  
+  const innerArray = [];
+  let cum = 0;
+  year.values.sort((a,b) => moment(a.key) - moment(b.key)).forEach(date => {
+    let yoy = undefined 
+    if(year.key == '2019') {
+      lyLookup[date.key.replace("-2019", "")] = date.value;
+    } else {
+      if(lyLookup[date.key.replace("-2020", "")] != undefined) {
+        yoy = date.value - lyLookup[date.key.replace("-2020", "")]
+      } 
+    }
+    cum += date.value;
+    innerArray.push({
+      key: date.key,
+      lookupKey: date.key.replace("2020", "2019"),
+      value: date.value,
+      cum: cum,
+      yoy: yoy,
+      roll: isNaN(mvData[`$${date.key}`]) ? 0 : mvData[`$${date.key}`]
+    })
+  });
+  
+  returnArray.push({
+    key: year.key,
+    values: innerArray
+  })
+  
+})
+
+return returnArray;
+}
